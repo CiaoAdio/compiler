812684b005d24fe032210781680356d129938376
Daniel Shao <sxy.suzhou@gmail.com>
2019-10-08 19:46:12 +0800

    [LAB1] Final implementation for Lab 1

--- common/common.c
+++ common/common.c
@@ -1,8 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
 #include "common.h"
 
 int getAllTestcase(char filename[][256])
 {
-	/// \todo student should fill this function
-	return 0;
+	int files_count = 0;
+	DIR* d;
+	struct dirent *file;
+	struct stat sb;
+	if(!(d = opendir("./testcase/"))){
+		printf("error opendir !\n");
+	}
+	while((file = readdir(d)) != NULL){
+		if(strncmp(file->d_name, ".", 1) == 0) continue;
+		strcpy(filename[files_count++], file->d_name);
+		if(stat(file->d_name, &sb) >= 0 && S_ISDIR(sb.st_mode)) continue;
+	}
+	closedir(d);
+	return files_count;
 }
 
